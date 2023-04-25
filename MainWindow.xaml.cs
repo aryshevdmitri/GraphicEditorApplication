c68204d292ebd678c446da9720d7e3a478359f8a
Aryshev Dmitry <aryshevdmitri@yandex.ru>
2023-04-26 02:40:26 +0500

    added some features

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -9,9 +11,14 @@ namespace GraphicEditorApplication {
             InitializeComponent();
         }
 
+        // режим работы графического редактора
         private string? mode = null;
-        private Point startPoint, endPoint; 
-
+        // точки отрезка
+        private Point startPoint, endPoint;
+        // динамический массив отрезков
+        private List<Line> lines = new();
+        // переменная для хранения выбранного отрезк
+        private Line? selectedLine = null; 
 
         private void btnCreate_Click(object sender, RoutedEventArgs e) => mode = "create";
 
@@ -25,6 +32,10 @@ namespace GraphicEditorApplication {
                     startPoint = e.GetPosition(canvas);
 
                     break;
+                case "edit":
+
+
+                    break;
             }
         }
 
@@ -33,20 +44,42 @@ namespace GraphicEditorApplication {
                 case "create":
                     endPoint = e.GetPosition(canvas);
 
-                    Line newLine = new() {
-                        X1 = startPoint.X, Y1 = startPoint.Y,
-                        X2 = endPoint.X, Y2 = endPoint.Y,
-
-                        Stroke = Brushes.Black, StrokeThickness = 2
-                    };
-
+                    Line newLine = CreateLine(startPoint, endPoint, Brushes.Black, 2);
                     canvas.Children.Add(newLine);
+                    lines.Add(newLine);
 
                     startPoint.X = 0; startPoint.Y = 0;
                     endPoint.X = 0; endPoint.Y = 0;
 
                     break;
+                case "edit":
+
+
+                    break;
             }
+        }
+
+        /// <summary>
+        /// Создание отрезка
+        /// </summary>
+        /// <param name="startPoint">Начальная точка отрезка</param>
+        /// <param name="endPoint">Конечная точка отрезка</param>
+        /// <param name="brushColor">Цвет кисти</param>
+        /// <param name="strokeThickness">Толщина кисти</param>
+        /// <returns>Line newLine</returns>
+        private static Line CreateLine(Point startPoint, Point endPoint, Brush brushColor, int strokeThickness) {
+            Line newLine = new() {
+                X1 = startPoint.X,
+                Y1 = startPoint.Y,
+                X2 = endPoint.X,
+                Y2 = endPoint.Y,
+                
+                Stroke = brushColor,
+                StrokeThickness = strokeThickness
+            };
+
+            return newLine;
         }
     }
 }
+
